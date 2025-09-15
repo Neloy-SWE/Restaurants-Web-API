87f496f696a0e13bbaeab7f2b8f1d7bc4da7a0a1
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-09-15 21:18:47 +0600

    restaurant repository added

---
+++ Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Restaurants.Infrastructure.Persistence;
+using Restaurants.Domain.Repositories;
+using Restaurants.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Restaurants.Infrastructure.Repositories
+{
+    internal class RestaurantsRepository(RestaurantsDbContext dbContext) : IRestaurantsRepository
+    {
+        public async Task<IEnumerable<Restaurant>> GetAllAsync()
+        {
+            var restaurants = await dbContext.Restaurants.ToListAsync();
+            return restaurants;
+        }
+    }
+}
