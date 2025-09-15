ddbb0044b794c562d4fd1560a912fa00eb1e2b04
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-09-16 02:03:55 +0600

    dto implemented

--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using System.IO;
 
 namespace Restaurants.Application.Restaurants
 {
     internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger) : IRestaurantsService
     {
-        public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+        public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
         {
             logger.LogInformation("Getting all restaurants");
             var restaurants = await restaurantsRepository.GetAllAsync();
-            return restaurants;
+            var restaurantsDtos = restaurants.Select(RestaurantDto.FromEntity);
+            return restaurantsDtos!;
+        }
+
+        public async Task<RestaurantDto?> GetById(int id)
+        {
+            logger.LogInformation($"Getting restaurant {id}");
+            var rastaurant = await restaurantsRepository.GetByIdAsync(id);
+            var restaurantDto = RestaurantDto.FromEntity(rastaurant);
+
+            return restaurantDto;
         }
     }
 }
