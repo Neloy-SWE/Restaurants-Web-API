0a7dd1551bfac72dd8b187ed506e07504c16842f
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-09-19 18:20:31 +0600

    validation behavior added

---
+++ Restaurants.Application/Restaurants/Behavior/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Restaurants.Application.Restaurants.Validators
+{
+    public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var failures = validators
+                    .Select(v => v.Validate(context))
+                    .SelectMany(result => result.Errors)
+                    .Where(f => f != null)
+                    .ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
+                    
+            }
+
+            return await next();
+        }
+    }
+}
