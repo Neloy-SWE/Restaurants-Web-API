bc5d2484a54dbac9ccf776c6fdcfa5197db99c81
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-09-19 18:18:28 +0600

    get all restaurants query, handler added

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
+{
+    internal class GetAllRestaurantsQueryHandler(IRestaurantsRepository restaurantsRepository, ILogger<GetAllRestaurantsQueryHandler> logger, IMapper mapper) : IRequestHandler<GetAllRestaurantsQuery, IEnumerable<RestaurantDto>>
+    {
+        public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting all restaurants");
+            var restaurants = await restaurantsRepository.GetAllAsync();
+
+            //var restaurantsDtos = restaurants.Select(RestaurantDto.FromEntity); // manual mapping
+            var restaurantsDtos = mapper.Map<IEnumerable<RestaurantDto>>(restaurants); // using AutoMapper
+
+            return restaurantsDtos!;
+        }
+    }
+}
