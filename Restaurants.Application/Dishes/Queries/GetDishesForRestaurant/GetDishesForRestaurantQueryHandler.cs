fdb48f6b210ee458e08261a404ee4e13c2561caa
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-10-02 20:23:53 +0600

    get all dishes for restaurant api added

---
+++ Restaurants.Application/Dishes/Queries/GetDishesForRestaurant/GetDishesForRestaurantQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Dishes.Queries.GetDishesForRestaurant
+{
+    public class GetDishesForRestaurantQueryHandler(ILogger<GetDishesForRestaurantQueryHandler> logger, IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<GetDishesForRestaurantQuery, IEnumerable<DishDto>>
+    {
+        public async Task<IEnumerable<DishDto>> Handle(GetDishesForRestaurantQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting dishes for restaurant with id: {RestaurantId}", request.RestaurantId);
+            var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId)
+                ?? throw new KeyNotFoundException($"Restaurant with id {request.RestaurantId} not found");
+
+            var result = mapper.Map<IEnumerable<DishDto>>(restaurant.Dishes);
+
+            return result;
+
+        }
+    }
+}
